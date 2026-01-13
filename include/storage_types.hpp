9725a1a5fef1b19aed9c5a634221171ed776fe33
Michał Szyszło <michalszyszlo05@gmail.com>
2026-01-13 18:15:45 +0100

    storage_types.hpp headers and types headers + UML diagram

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -4,5 +4,34 @@
 
 #ifndef NETSIM_STORAGE_TYPES_HPP
 #define NETSIM_STORAGE_TYPES_HPP
+#include <cstddef>
+#include <list>
+#include "package.hpp"
+
+using const_iterator = std::list<Package>::const_iterator;
+
+enum class PackageQueueType {
+    FIFO,
+    LIFO
+};
+
+class IPackageStockpile {
+    public:
+        virtual ~IPackageStockpile() {}
+        virtual void push(Package&& p) = 0;
+        virtual bool empty() const = 0;
+        virtual size_t size();
+        virtual const_iterator begin() const = 0;
+        virtual const_iterator end() const = 0;
+};
+
+class IPackageQueue : public IPackageStockpile{
+public:
+    ~IPackageQueue() override = default;
+
+    virtual Package pop() = 0;
+    virtual PackageQueueType get_queue_type() const = 0;
+};
+
 
 #endif //NETSIM_STORAGE_TYPES_HPP
--- include/types.hpp
+++ include/types.hpp
@@ -4,7 +4,11 @@
 
 #ifndef NETSIM_TYPES_HPP
 #define NETSIM_TYPES_HPP
+#include <functional>
 
-using ElementID = unsigned long long;
+using ElementID = int;
+using Time = int;
+using TimeOffset = int;
+using ProbabilityGenerator = std::function<double()>;
 
 #endif //NETSIM_TYPES_HPP
