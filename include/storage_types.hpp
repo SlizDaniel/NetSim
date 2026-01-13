dd9331ae106113f5b4d4fcf784656c5c18257a1c
Michał Szyszło <michalszyszlo05@gmail.com>
2026-01-13 21:05:17 +0100

    storage_types headers and cpp files done, tests_storage_types paased

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -20,9 +20,11 @@ class IPackageStockpile {
         virtual ~IPackageStockpile() {}
         virtual void push(Package&& p) = 0;
         virtual bool empty() const = 0;
-        virtual size_t size();
+        virtual size_t size() const = 0;
         virtual const_iterator begin() const = 0;
         virtual const_iterator end() const = 0;
+        virtual const_iterator cbegin() const = 0;
+        virtual const_iterator cend() const = 0;
 };
 
 class IPackageQueue : public IPackageStockpile{
@@ -33,5 +35,38 @@ public:
     //virtual PackageQueueType get_queue_type() const = 0;
 };
 
+class PackageQueue : public IPackageQueue {
+public:
+    explicit PackageQueue(PackageQueueType type) : queue_type_(type) {};
+    ~PackageQueue() override = default;
+
+    Package pop() override;
+
+    void push(Package&& p) override {
+        this -> package_list_.emplace_back(std::move(p));
+    };
+    bool empty() const override {
+        return this -> package_list_.empty();
+    };
+    size_t size() const override {
+        return this -> package_list_.size();
+    };
+
+    const_iterator begin() const override {
+        return package_list_.begin();
+    };
+    const_iterator end() const override {
+        return package_list_.end();
+    };
+    const_iterator cbegin() const override {
+        return package_list_.cbegin();
+    };
+    const_iterator cend() const override {
+        return package_list_.cend();
+    };
+private:
+    std::list<Package> package_list_;
+    PackageQueueType queue_type_;
+};
 
 #endif //NETSIM_STORAGE_TYPES_HPP
--- src/storage_types.cpp
+++ src/storage_types.cpp
@@ -1,3 +1,20 @@
 //
 // Created by slizd on 13.01.2026.
 //
+
+#include "storage_types.hpp"
+
+Package PackageQueue::pop() {
+    Package package;
+
+    if (this -> queue_type_ == PackageQueueType::LIFO) {
+        package = std::move(this -> package_list_.back());
+        this -> package_list_.pop_back();
+    }
+    else {
+        package = std::move(this -> package_list_.front());
+        this -> package_list_.pop_front();
+    }
+
+    return package;
+}
