0d33d16ade8b023cf89609d4e19df190e87c3817
SlizDaniel <slizdaniel122@gmail.com>
2026-01-14 17:22:48 +0100

    Implemented class IPackageReceiver header

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -5,4 +5,27 @@
 #ifndef NETSIM_NODES_HPP
 #define NETSIM_NODES_HPP
 
+#include "storage_types.hpp"
+
+class IPackageReceiver{
+
+public:
+    virtual void receive_package(Package&& p) = 0;
+
+    virtual void get_id(ElementID) const = 0;
+
+    virtual const_iterator begin() const = 0;
+
+    virtual const_iterator end() const = 0;
+
+    virtual const_iterator cbegin() const = 0;
+
+    virtual const_iterator cend() const = 0;
+
+    virtual ~IPackageReceiver() = default;
+};
+
+class ReceiverPreferences : IPackageReceiver {
+public:
+};
 #endif //NETSIM_NODES_HPP
